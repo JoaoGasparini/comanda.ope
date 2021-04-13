64c196c2822e385ee8624cda1a8139cb38f8a44a
Kakarotto8000 <victormunaro@gmail.com>
2021-04-13 19:13:56 -0300

    Alterações layout
    Alterações layout

--- comandaOpe/comandaOpe/Controllers/UsuarioController.cs
+++ comandaOpe/comandaOpe/Controllers/UsuarioController.cs
@@ -4,6 +4,9 @@ using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
 using System.Threading.Tasks;
+using comandaOpe.Data;
+using comandaOpe.Data.Models;
+using System.Linq;
 
 namespace comandaOpe.Controllers
 {
@@ -35,11 +38,32 @@ namespace comandaOpe.Controllers
             return View();
         }
 
+        public IActionResult Painel()
+        {
+            return View("PainelAdm", "Usuario");
+        }
+
         [HttpGet]
         public async Task<IActionResult> Logout()
         {
             await HttpContext.SignOutAsync();
             return RedirectToAction("UsuarioLogin", "Login");
         }
+
+        //public IActionResult CriarComanda()
+        //{
+        //    DbSistemaComandaContext contexto = new DbSistemaComandaContext();
+        //    Pedido pedidoe = new Pedido();
+        //    pedidoe
+        //    ComandaModel comanda = new ComandaModel();
+        //    Comanda comandae = new Comanda();
+        //    comandae.id_cliente = new UsuarioModel().Listar().Where(p => p.login == "victor").FirstOrDefault().id;
+        //    comandae.forma_pagamento = Request.Form["formapagamento"];
+        //    comanda.Inserir(comandae);
+
+
+        //}
     }
+
+    
 }
